29365ad154cc005f8e85ec2c3255a122b7b77ffc
huangcheng <3103720798@qq.com>
2022-10-05 11:54:22 +0800

    同步数据库,新增组织ID属性

--- src/main/java/run/hxtia/workbd/pojo/vo/response/AdminUserVo.java
+++ src/main/java/run/hxtia/workbd/pojo/vo/response/AdminUserVo.java
@@ -12,6 +12,9 @@ public class AdminUserVo {
     @ApiModelProperty("用户ID")
     private Long id;
 
+    @ApiModelProperty("组织ID")
+    private short orgId;
+
     @ApiModelProperty("用户名")
     private String username;
 
