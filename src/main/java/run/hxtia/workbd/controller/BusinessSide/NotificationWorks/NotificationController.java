64c54ba2be6234fa88c0811e98fb901516ef8f79
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-05-10 07:56:51 +0800

    NotificationController modify

--- src/main/java/run/hxtia/workbd/controller/BusinessSide/NotificationWorks/NotificationController.java
+++ src/main/java/run/hxtia/workbd/controller/BusinessSide/NotificationWorks/NotificationController.java
@@ -33,8 +33,8 @@ public class NotificationController {
 
     @PostMapping("/list")
     @ApiOperation(value = "分页查询通知")
-    public PageVo list(@RequestBody NotificationPageReqVo pageReqVo, @RequestParam String type) {
-        return notificationService.list(pageReqVo, type);
+    public PageVo list(@RequestBody NotificationPageReqVo pageReqVo) {
+        return notificationService.list(pageReqVo, pageReqVo.getType());
     }
 
     @PostMapping("/saveOrUpdate")
